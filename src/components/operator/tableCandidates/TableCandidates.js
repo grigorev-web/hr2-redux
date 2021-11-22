8d030039e3901c6ed15fff452f8d5e8eca805faf
dgrigorev <dm-grig@mail.ru>
2021-11-22 11:52:43 +0300

    1

--- src/components/operator/tableCandidates/TableCandidates.js
+++ src/components/operator/tableCandidates/TableCandidates.js
@@ -1,32 +1,30 @@
-import {useEffect, useMemo} from 'react';
-import {useSelector, useDispatch} from 'react-redux';
+import { useEffect, useMemo } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-import {asyncGetCandidatesInit} from '../../../store/asyncActions';
+import { asyncGetCandidatesInit } from "../../../store/asyncActions";
 
-import TableCandidatesLayout from "../../layout/TableCandidatesLayout";
+import TableCandidatesLayout from "./layout/TableCandidatesLayout";
 import TableCandidatesHead from "./TableCandidatesHead";
 import TableCandidatesRow from "./TableCandidatesRow";
 
-
 const TableCandidates = () => {
-
-  const candidates = useSelector( state => state.candidates);
+  const candidates = useSelector((state) => state.candidates);
   const dispatch = useDispatch();
 
-
-  useEffect(()=>{
+  useEffect(() => {
     dispatch(asyncGetCandidatesInit());
-  },[]);
+  }, []);
 
-
-  if(!candidates.length) return <h2>Нет кандидатов</h2>;
+  if (!candidates.length) return <h2>Нет кандидатов</h2>;
 
   return (
     <TableCandidatesLayout>
       <table className="align-middle mb-0 table table-borderless table-striped table-hover">
         <TableCandidatesHead />
         <tbody>
-          {candidates.map( candidate => <TableCandidatesRow candidate={candidate} key={candidate.id}/> )}
+          {candidates.map((candidate) => (
+            <TableCandidatesRow candidate={candidate} key={candidate.id} />
+          ))}
         </tbody>
       </table>
     </TableCandidatesLayout>
